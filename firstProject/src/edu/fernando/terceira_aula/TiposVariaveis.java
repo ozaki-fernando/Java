92f94b15235241708b4e2f1a1b25c136823767dd
Fernando Ozaki <ozak.pgm@gmail.com>
2023-03-02 22:04:46 -0300

    ALTERAÇÃO ARQUIVO TiposVariaveis

--- firstProject/src/edu/fernando/terceira_aula/TiposVariaveis.java
+++ firstProject/src/edu/fernando/terceira_aula/TiposVariaveis.java
@@ -2,6 +2,15 @@ package edu.fernando.terceira_aula;
 
 public class TiposVariaveis {
     public static void main(String[] args) {
-        
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        final int NUMERO = 1;
+        //NUMERO = 2;
+
+
     }
 }
